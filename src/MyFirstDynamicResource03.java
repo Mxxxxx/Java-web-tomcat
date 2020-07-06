8da774ef4082e4e357ab40811953c27fa73de24a
孟鑫 <907008127@qq.com>
2020-07-06 17:36:43 +0800

    e

---
+++ src/MyFirstDynamicResource03.java
@@ -0,0 +1,22 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @program: 第一组动态资源
+ * @description:
+ * @author: Mx
+ * @create: 2020-07-06 17:28
+ **/
+public class MyFirstDynamicResource03 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String text = req.getParameter("text");
+        resp.setContentType("text/html; charset=utf-8");
+        PrintWriter writer = resp.getWriter();
+        writer.println("<h1> 我text </h1>");
+    }
+}
